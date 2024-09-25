f6a5a10d28c8fcf4537364f55d73f186f2ddf001
keaimk <fly1043@naver.com>
2024-09-25 12:35:15 +0900

    view

--- src/main/java/com/example/spring/controller/Controller06.java
+++ src/main/java/com/example/spring/controller/Controller06.java
@@ -24,6 +24,7 @@ public class Controller06 {
         return html;
     }
 
+//    /main6/sub2
     @RequestMapping("sub2")
     public String method2() {
 //        1. 요청 정보 분석/기겅
@@ -37,5 +38,47 @@ public class Controller06 {
 
 //        최종 jsp 위치
 //        /WEB-INF/view/world.jsp
+    }
+
+    @RequestMapping("sub3")
+    public String method3() {
+
+//        /WEB-INF/view/hello.jsp
+        return "hello";
+    }
+
+//    /main6/sub4 으로 요청 왔을 때
+//    /WEB-INF/view/hi.jsp 의 내용이 응답
+
+//    메소드 작성, jsp 만들기
+
+    @RequestMapping("sub4")
+    public String method4() {
+
+//        /WEB-INF/view/hi.jsp
+        return "hi";
+    }
+
+//    /main6/sub5
+    @RequestMapping("sub5")
+    public String method5() {
+
+//        jsp의 위치는 요청 경로와 같은 경우가 많음
+//        /WEB-INF/view/main6/sub5.jsp
+        return "/main6/sub5";
+    }
+
+    @RequestMapping("sub7")
+    public void method7() {
+//        return이 void면 요청 경로와 같은 view로 결정
+//        /WEB-INF/view/main6/sub7.jsp
+    }
+
+//      /main6/sub8 로 요청 왔을 때
+//      /WEB-INF/view/main6/sub8.jsp 가 응답되도록
+
+//      8번째 메소드와, jsp를 작성해보세요.
+    @RequestMapping("sub8")
+    public void method8() {
     }
 }

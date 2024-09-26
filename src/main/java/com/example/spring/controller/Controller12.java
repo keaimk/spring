5c3c46fa90b112b4a1f7a64ba388e862ca94fad9
keaimk <fly1043@naver.com>
2024-09-26 16:36:53 +0900

    model, el

--- src/main/java/com/example/spring/controller/Controller12.java
+++ src/main/java/com/example/spring/controller/Controller12.java
@@ -1,6 +1,9 @@
 package com.example.spring.controller;
 
 import com.example.spring.dto.c12.Book;
+import com.example.spring.dto.c12.Car;
+import com.example.spring.dto.c12.Product;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,44 @@ public class Controller12 {
         Book book2 = new Book("java", "shin", 600);
 
         model.addAttribute("books", List.of(book, book2));
+        return null;
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(Model model) {
+        Car car = new Car();
+        car.setMake("tesla");
+        car.setYear(2024);
+        car.setModel("truck");
+
+        car.getMake();
+        car.getYear();
+        car.getModel();
+
+        System.out.println(car);
+
+        return null;
+    }
+
+    @RequestMapping("sub3")
+    public String sub3(Model model) {
+
+        model.addAttribute("num1", 99);
+        model.addAttribute("num2", 88);
+
+        return null;
+    }
+
+    @RequestMapping("sub4")
+    public String sub4(Model model) {
+        model.addAttribute("productList", List.of(
+                new Product(3, "컴퓨터", 500, 1, "인텔 컴퓨터"),
+                new Product(6, "스마트폰", 600, 3, "아이폰"),
+                new Product(7, "자동차", 700, 0, "테슬라"),
+                new Product(12, "모니터", 200, 6, "LG FHD")
+
+        ));
+
         return null;
     }
 }

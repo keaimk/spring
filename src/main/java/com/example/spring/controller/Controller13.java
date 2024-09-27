fb165ebe36e91e4e66ce7bb2dcf6de03249dbb1f
keaimk <fly1043@naver.com>
2024-09-27 10:52:40 +0900

    if var

---
+++ src/main/java/com/example/spring/controller/Controller13.java
@@ -0,0 +1,28 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main13")
+public class Controller13 {
+
+    @RequestMapping("sub1")
+    public void method1(Model model) {
+        model.addAttribute("attr1", "hello model attribute");
+
+    }
+
+    @RequestMapping("sub2")
+    public void method2(Model model) {
+    }
+
+    @RequestMapping("sub3")
+    public void method3(Model model) {
+    }
+
+    @RequestMapping("sub4")
+    public void method4(Model model) {
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main13/sub4.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<%-- var 속성의 이름으로 test 연산 결과가 el 객체(attribute)에 저장됨 --%>
+<c:if test="${param.age < 21}" var="forKids">
+    <p>어린이용 컨텐츠1</p>
+</c:if>
+<c:if test="${param.age >= 21}" var="forAdults">
+    <p>성인용 컨텐츠1</p>
+</c:if>
+<hr>
+<c:if test="${forKids}">
+    <p>어린이용 컨텐츠2</p>
+</c:if>
+<c:if test="${forAdults}">
+    <p>성인용 컨텐츠2</p>
+</c:if>
+</body>
+</html>

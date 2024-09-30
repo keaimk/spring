45121f68cb21bb0fe03d0db28fe49663068fb014
keaimk <fly1043@naver.com>
2024-09-30 16:41:47 +0900

    redirect

---
+++ src/main/java/com/example/spring/controller/Controller17.java
@@ -0,0 +1,25 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main17")
+public class Controller17 {
+    // Controller16, sub3,sub4,sub5,sub9 가 하는 일 작성
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

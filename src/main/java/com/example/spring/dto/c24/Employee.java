bad30a69188a1f98e8e7ec52f8adc1b40055233a
keaimk <fly1043@naver.com>
2024-10-07 15:18:49 +0900

    직원 정보

---
+++ src/main/java/com/example/spring/dto/c24/Employee.java
@@ -0,0 +1,11 @@
+package com.example.spring.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String employeeID;
+    private String firstName;
+    private String lastName;
+    private String BirthDate;
+}

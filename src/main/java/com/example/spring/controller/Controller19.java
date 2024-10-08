cb9133582279b4f29a91aa3312ab4608c66e0a93
keaimk <fly1043@naver.com>
2024-10-08 14:55:02 +0900

    form

---
+++ src/main/java/com/example/spring/controller/Controller19.java
@@ -0,0 +1,43 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+@RequestMapping("main19")
+public class Controller19 {
+
+    @RequestMapping("sub1")
+    public void sub1(String address, String nick) {
+        System.out.println("address = " + address);
+        System.out.println("nick = " + nick);
+    }
+
+    @RequestMapping("sub2")
+    public void sub2(String name, Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+
+//    sub1.jsp 에 새 form 요소(와 input 요소들)을 작성해서
+//    전송 버튼을 클릭하면
+//    /main19/sub3?food=피자&email=yahoo&city=인천
+//    요청되게 작성하고
+
+    //    Controller19.sub3 메소드 작성해서 food, email, city
+//    요청 파라미터를 받아서 출력하는 코드 작성
+    @RequestMapping("sub3")
+    public void sub3(String food, String email, String city) {
+        System.out.println("food = " + food);
+        System.out.println("email = " + email);
+        System.out.println("city = " + city);
+    }
+
+    @RequestMapping("sub4")
+    public void sub4(String food, String name, String memo) {
+        System.out.println("food = " + food);
+        System.out.println("name = " + name);
+        System.out.println("memo = " + memo);
+    }
+}

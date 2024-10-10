5e7644f25ebb36d33236d6c0194535f16882d7ae
keaimk <fly1043@naver.com>
2024-10-10 12:20:52 +0900

    paging

---
+++ src/main/java/com/example/spring/controller/Controller27.java
@@ -0,0 +1,220 @@
+package com.example.spring.controller;
+
+import com.example.spring.dto.c26.Customer;
+import com.example.spring.dto.c27.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("main27")
+public class Controller27 {
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("sub1")
+    public void sub1(Model model, String id) throws SQLException {
+        // SQL injection
+        String sql = STR."""
+                SELECT *
+                FROM Customers
+                WHERE CustomerId = \{id}
+                """;
+        Connection conn = dataSource.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+        try (conn; stmt; rs) {
+            List<Customer> list = new ArrayList<>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setPostalCode(rs.getString("PostalCode"));
+                customer.setName(rs.getString("CustomerName"));
+                customer.setContact(rs.getString("ContactName"));
+                customer.setAddress(rs.getString("Address"));
+                customer.setCity(rs.getString("City"));
+                customer.setId(rs.getString("CustomerId"));
+                customer.setCountry(rs.getString("Country"));
+                list.add(customer);
+            }
+            model.addAttribute("customerList", list);
+        }
+    }
+
+    // Statement 객체는 SQL injection 위험 있음
+    // -> PreparedStatement 객체를 사용해야 함
+    @GetMapping("sub2")
+    public String sub2(Model model, String id) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM Customers
+                WHERE CustomerId = ?
+                """;
+
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, id);
+        ResultSet rs = pstmt.executeQuery();
+
+        try (conn; pstmt; rs) {
+            List<Customer> list = new ArrayList<>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setPostalCode(rs.getString("PostalCode"));
+                customer.setName(rs.getString("CustomerName"));
+                customer.setContact(rs.getString("ContactName"));
+                customer.setAddress(rs.getString("Address"));
+                customer.setCity(rs.getString("City"));
+                customer.setCountry(rs.getString("Country"));
+                customer.setId(rs.getString("CustomerId"));
+                list.add(customer);
+            }
+            model.addAttribute("customerList", list);
+        }
+        return "/main27/sub1";
+    }
+
+    //    /main26/sub5
+//    /main27/sub3
+    @GetMapping("sub3")
+    public String sub3(Model model, String from, String to) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM Products
+                WHERE Price BETWEEN ? AND ?
+                ORDER BY Price
+                """;
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, from);
+        pstmt.setString(2, to);
+        ResultSet rs = pstmt.executeQuery();
+
+        try (conn; pstmt; rs) {
+            List<Customer> list = new ArrayList<>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setPostalCode(rs.getString("PostalCode"));
+                customer.setName(rs.getString("CustomerName"));
+                customer.setContact(rs.getString("ContactName"));
+                customer.setAddress(rs.getString("Address"));
+                customer.setCity(rs.getString("City"));
+                customer.setCountry(rs.getString("Country"));
+                customer.setId(rs.getString("CustomerId"));
+                list.add(customer);
+            }
+            model.addAttribute("customerList", list);
+        }
+        return "/main26/sub5";
+    }
+
+    @GetMapping("sub4")
+    public void sub4(Model model, String begin, String end) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM Orders
+                WHERE OrderDate BETWEEN ? AND ?
+                ORDER BY OrderDate
+                """;
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, begin);
+        pstmt.setString(2, end);
+        ResultSet rs = pstmt.executeQuery();
+
+        try (conn; pstmt; rs) {
+            List<Order> list = new ArrayList<>();
+            while (rs.next()) {
+                Order order = new Order();
+                order.setId(rs.getString("orderId"));
+                order.setCustomerID(rs.getString("CustomerID"));
+                order.setEmployeeID(rs.getString("EmployeeID"));
+                order.setOrderDate(rs.getString("OrderDate"));
+                order.setShipperID(rs.getString("shipperID"));
+                list.add(order);
+            }
+            model.addAttribute("orderList", list);
+        }
+    }
+
+    @GetMapping("sub5")
+    public void sub5(Model model,
+                     @RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
+                     @RequestParam(value = "page_count", defaultValue = "10") Integer pageCount) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM Customers
+                ORDER BY CustomerId
+                LIMIT ?, ?
+                """;
+
+        Integer offset = (pageNumber - 1) * pageCount;
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, offset);
+        pstmt.setInt(2, pageCount);
+        ResultSet rs = pstmt.executeQuery();
+
+        try (conn; pstmt; rs) {
+            List<Customer> list = new ArrayList<>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setPostalCode(rs.getString("PostalCode"));
+                customer.setName(rs.getString("CustomerName"));
+                customer.setContact(rs.getString("ContactName"));
+                customer.setAddress(rs.getString("Address"));
+                customer.setCity(rs.getString("City"));
+                customer.setCountry(rs.getString("Country"));
+                customer.setId(rs.getString("CustomerId"));
+                list.add(customer);
+            }
+            model.addAttribute("customerList", list);
+        }
+    }
+
+//    6번째 메소드
+//    최신 주문순 (ORDER BY OrderID DESC)
+//    각 페이지에 20개씩 주문이 조회되는 코드 작성
+
+    @GetMapping("sub6")
+    public void sub6(Model model,
+                     @RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
+                     @RequestParam(value = "page_count", defaultValue = "20") Integer pageCount) throws SQLException {
+        String sql = """
+                SELECT *
+                FROM Orders
+                ORDER BY OrderID DESC
+                LIMIT ?, ?
+                """;
+
+        Integer offset = (pageNumber - 1) * pageCount;
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, offset);
+        pstmt.setInt(2, pageCount);
+        ResultSet rs = pstmt.executeQuery();
+
+        try (conn; pstmt; rs) {
+            List<Order> list = new ArrayList<>();
+            while (rs.next()) {
+                Order order = new Order();
+                order.setId(rs.getString("orderId"));
+                order.setCustomerID(rs.getString("CustomerID"));
+                order.setEmployeeID(rs.getString("EmployeeID"));
+                order.setOrderDate(rs.getString("OrderDate"));
+                order.setShipperID(rs.getString("shipperID"));
+                list.add(order);
+            }
+            model.addAttribute("orderList", list);
+        }
+    }
+}

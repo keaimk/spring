bf4decdda1e37e0b97e8fe643b4667388fea3c5c
keaimk <fly1043@naver.com>
2024-10-22 17:35:37 +0900

    default

---
+++ sql/19default.sql
@@ -0,0 +1,33 @@
+USE db1;
+# DEFAULT : 값을 넣지 않을 때의 기본값
+CREATE TABLE my_table24
+(
+    col1 VARCHAR(1),
+    col2 VARCHAR(1) DEFAULT 'x'
+);
+
+SELECT *
+FROM my_table24;
+
+INSERT INTO my_table24
+    (col2) VALUE ('a');
+INSERT INTO my_table24
+    (col1) VALUE ('a');
+INSERT INTO my_table24
+    (col1, col2) VALUE ('a', NULL);
+DESC my_table24;
+
+CREATE TABLE my_table25
+(
+    col1 INT NOT NULL UNIQUE DEFAULT 0
+);
+
+# my_table26
+# ssn, name, age, address
+CREATE TABLE my_table26
+(
+    ssn     VARCHAR(13) NOT NULL UNIQUE,
+    name    VARCHAR(10) NOT NULL,
+    age     INT DEFAULT 0,
+    address VARCHAR(50)
+);

09fefde7bfb5798eeaec2892c8c45a7ceda3b312
keaimk <fly1043@naver.com>
2024-11-11 17:28:24 +0900

    요청 받기

--- src/main/java/com/example/spring/api/ApiController1.java
+++ src/main/java/com/example/spring/api/ApiController1.java
@@ -1,29 +1,64 @@
 package com.example.spring.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RequestMapping("/api/main1")
 @Controller
 public class ApiController1 {
-    @PostMapping("sub1")
-    public void sub1() {
-        System.out.println("/api/main1/sub1으로 요청옴");
+    @PostMapping("sub9")
+    public void sub9(@RequestBody List<Map<String, Object>> list) {
+        // [{product: {name: computer, price: 500}, company: samsung},
+        // {product: {name: smart phone, price: 1000}, company: apple},
+        // {product: {name: graphic card, price: 2000}, company: nvidia }]
+
+        System.out.println(list);
+    }
+    
+    @PostMapping("sub8")
+    public void sub8(@RequestBody List<Map<String, Object>> list) {
+        System.out.println(list);
+    }
+
+    @PostMapping("sub7")
+    public void sub6(@RequestBody List<String> list) {
+        System.out.println(list);
+    }
+
+    @PostMapping("sub6")
+    public void sub6(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+    }
+
+    @PostMapping("sub4")
+    public void sub4(@RequestBody Map<String, Object> map) {
+
+        // {title: 책제목, content: 책내용, price: 5000}
+        System.out.println(map);
+    }
+
+    @PostMapping("sub3")
+    public void sub3(@RequestBody Map<String, String> map) {
+        System.out.println(map);
     }
 
     // @RequestBody : JSON -> Map(JavaBeans)로 파싱해줌
     @PostMapping("sub2")
     public void sub2(@RequestBody Map<String, Object> map) {
+
         System.out.println(map);
         System.out.println("ApiController1.sub2");
     }
 
-    @PostMapping("sub3")
-    public void sub3(@RequestBody Map<String, Object> map) {
-        System.out.println(map);
+    @PostMapping("sub1")
+    public void sub1() {
+        System.out.println("/api/main1/sub1으로 요청옴");
     }
 }

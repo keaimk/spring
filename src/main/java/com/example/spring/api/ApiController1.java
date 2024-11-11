4974d22eee6121ffe8c34afc3a49d80ecb8fd42c
keaimk <fly1043@naver.com>
2024-11-11 15:42:55 +0900

    요청 받기

---
+++ src/main/java/com/example/spring/api/ApiController1.java
@@ -0,0 +1,29 @@
+package com.example.spring.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequestMapping("/api/main1")
+@Controller
+public class ApiController1 {
+    @PostMapping("sub1")
+    public void sub1() {
+        System.out.println("/api/main1/sub1으로 요청옴");
+    }
+
+    // @RequestBody : JSON -> Map(JavaBeans)로 파싱해줌
+    @PostMapping("sub2")
+    public void sub2(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+        System.out.println("ApiController1.sub2");
+    }
+
+    @PostMapping("sub3")
+    public void sub3(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+    }
+}

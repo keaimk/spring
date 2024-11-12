974066280d9f562b7686060553e04d410e83a4c8
keaimk <fly1043@naver.com>
2024-11-12 09:40:08 +0900

    json을 javaBean(dto)로 받기

--- src/main/java/com/example/spring/api/ApiController2.java
+++ src/main/java/com/example/spring/api/ApiController2.java
@@ -1,5 +1,6 @@
 package com.example.spring.api;
 
+import com.example.spring.dto.a2.Book;
 import com.example.spring.dto.a2.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,11 @@ import java.util.Map;
 @Controller
 @RequestMapping("/api/main2")
 public class ApiController2 {
+    @PostMapping("sub2")
+    public void method2(@RequestBody Book book) {
+        System.out.println(book);
+    }
+
     @PostMapping("sub1")
     public void method1(@RequestBody Person person) {
         System.out.println(person);

a6288978a0aff406ec3088124f22aa9ada676c18
keaimk <fly1043@naver.com>
2024-11-12 14:48:56 +0900

    response body

--- src/main/java/com/example/spring/api/ApiController5.java
+++ src/main/java/com/example/spring/api/ApiController5.java
@@ -1,5 +1,9 @@
 package com.example.spring.api;
 
+import com.example.spring.dto.a2.Book;
+import com.example.spring.dto.a5.Company;
+import com.example.spring.dto.a5.Member;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +12,61 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
-@RestController
 @RequestMapping("/api/main5")
+@Controller
 public class ApiController5 {
+    @GetMapping("sub10")
+    @ResponseBody
+    public List<Book> sub10() {
+        Book book1 = new Book();
+        book1.setTitle("Book 1");
+        book1.setAuthor("Author 1");
+        book1.setContent("Content 1");
+        book1.setPrice(3000);
+
+        Book book2 = new Book();
+        book2.setTitle("Book 2");
+        book2.setAuthor("Author 2");
+        book2.setContent("Content 2");
+        book2.setPrice(4000);
+
+        Book book3 = new Book();
+        book3.setTitle("Book 3");
+        book3.setAuthor("Author 3");
+        book3.setContent("Content 3");
+        book3.setPrice(5000);
+
+        return List.of(book1, book2, book3);
+    }
+
+    @GetMapping("sub9")
+    @ResponseBody
+    public Member sub9() {
+
+        Company company = new Company();
+        company.setName("토트넘");
+        company.setLocation("런던");
+
+        Member member = new Member();
+        member.setName("흥민");
+        member.setAddress("서울");
+        member.setMarried(true);
+        member.setItems(List.of("공", "신발", "유니폼"));
+        member.setCompany(company);
+        return member;
+    }
+
+    @GetMapping("sub8")
+    @ResponseBody
+    public Book sub8() {
+        Book book = new Book();
+        book.setTitle("제목");
+        book.setAuthor("한강");
+        book.setContent("소설");
+        book.setPrice(50000);
+        return book;
+    }
+
     @GetMapping("sub7")
     @ResponseBody
     public Map<String, Object> sub7() {
